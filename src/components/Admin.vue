050eff4c5fc039cf05f3d221e26823577c978a25
Murray Smith <msmith@smarttech.com>
2017-06-09 20:37:48 -0600

    get basic admin tables up and running

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -13,22 +13,23 @@
       <table>
         <thead>
           <tr>
-            <th v-for="key of asTableData(timeslots)[0]">{{ key }}</th>
+            <th v-for="key of asTableData(adminTimeslots)[0]">{{ key }}</th>
           </tr>
         </thead>
         <thead>
-          <tr v-for="row of asTableData(timeslots).slice(1)">
+          <tr v-for="row of asTableData(adminTimeslots).slice(1)">
             <td v-for="val of row">{{ val }}</td>
           </tr>
         </thead>
-      </table>      <table>
+      </table>
+      <table>
         <thead>
           <tr>
-            <th v-for="key of asTableData(dancersRaw)[0]">{{ key }}</th>
+            <th v-for="key of asTableData(adminDancers)[0]">{{ key }}</th>
           </tr>
         </thead>
         <thead>
-          <tr v-for="row of asTableData(dancersRaw).slice(1)">
+          <tr v-for="row of asTableData(adminDancers).slice(1)">
             <td v-for="val of row">{{ val }}</td>
           </tr>
         </thead>
@@ -38,9 +39,10 @@
 </template>
 
 <script>
+import moment from 'moment';
 import {
   // firebase,
-  // idKey,
+  idKey,
   db,
   dba,
 } from '../helpers/firebase';
@@ -60,46 +62,86 @@ export default {
     contactsRaw: dba.child('contacts'),
     usersRaw: db.child('users'),
   },
-  // computed: {
-  //   users() {
-  //     return this.usersRaw.map(($item) => {
-  //       const item = {
-  //         ...$item,
-  //       };
-  //       return item;
-  //     });
-  //   },
-  //   dancersRaw() {
-  //     const dancersRaw = [];
-  //     this.users.forEach((user) => {
-  //       dancersRaw.splice(0, 0, user.dancers);
-  //     });
-  //     return dancersRaw;
-  //   },
-  //   adminTimeslots() {
-  //     return this.timeslots.map(($item) => {
-  //       const item = {
-  //         ...$item,
-  //       };
-  //       item.$dancers = this.dancers
-  //         .filter(d => Object.keys(item['@dancers']).includes(d[idKey]));
-  //       return item;
-  //     });
-  //   },
-  // },
+  computed: {
+    adminTimeslots() {
+      return this.timeslots.map(($item) => {
+        const item = {
+          ...$item,
+        };
+        item.dancers = this.dancers
+          .filter(d => Object.keys(item['@dancers']).includes(d[idKey]))
+          .map(d => d.name);
+        item.classes = item.$classes.map(c => c.name);
+        item.locations = item.$locations.map(l => l.nickname);
+
+        delete item.$capacity;
+        delete item.$name;
+        delete item['@dancers'];
+        delete item['@classes'];
+        delete item.$classes;
+        delete item['@locations'];
+        delete item.$locations;
+        delete item.props;
+        return item;
+      }).sort((a, b) => (a.startDay < b.startDay ? -1 : 1));
+    },
+    adminDancers() {
+      return this.dancers.map(($item) => {
+        const item = {
+          ...$item,
+          birthday: moment($item.birthday).format('MMMM d, YYYY'),
+        };
+        item.timeslots = this.timeslots
+            .filter(t => Object.keys(item['@timeslots']).includes(t[idKey]))
+            .map(t => `${moment().day(t.startDay).format('dddd')} @ ${t.startTime}`);
+
+        delete item['@timeslots'];
+        delete item['@users'];
+        return item;
+      });
+    },
+    dancers() {
+      return this.dancersRaw.map(($item) => {
+        const item = {
+          '@timeslots': {},
+          '@users': {},
+          ...$item,
+        };
+        return item;
+      });
+    },
+    contacts() {
+      return this.contactsRaw.map(($item) => {
+        const item = {
+          '@users': {},
+          ...$item,
+        };
+        return item;
+      });
+    },
+    users() {
+      return this.usersRaw.map(($item) => {
+        const item = {
+          ...$item,
+        };
+        return item;
+      });
+    },
+  },
   methods: {
-    asTableData(arrayOfObjects) {
+    getCollectionKeys(arrayOfObjects) {
       const keys = [].concat.apply([], arrayOfObjects // eslint-disable-line prefer-spread
         .map(item => Object.keys(item)))
         .filter((v, i, a) => a.indexOf(v) === i)
         .sort();
 
+      return keys;
+    },
+    asTableData(arrayOfObjects) {
+      const keys = this.getCollectionKeys(arrayOfObjects);
+
       return [keys].concat(arrayOfObjects.map(item => [].concat(keys).map(key => item[key])));
     },
-
-    // resolveRelations(item) {
-
-    // },
 
     handleTimeslotSelect(e, timeslot) {
       console.log(timeslot);
