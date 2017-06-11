370f12df9712a8f31364c25cfa209981a1786f68
Murray Smith <msmith@smarttech.com>
2017-06-11 13:06:20 -0600

    debounce firebase functions

--- functions/index.js
+++ functions/index.js
@@ -14,6 +14,7 @@ const dba = firebase.database().ref(`${ENV}/admin`);
 const db = firebase.database().ref(`${ENV}/data`);
 
 const moment = require('moment');
+const debounce = require('throttle-debounce/debounce');
 const renderEmail = require('./helpers/renderEmail');
 const sendEmail = require('./helpers/sendEmail');
 
@@ -59,95 +60,92 @@ function getUsersEnrolledDancers(userId) {
 
 if (DEBUG) return; // @DEBUG
 
-exports.syncAdminItems = functions.database.ref(`/${ENV}/data/users/{userId}/{collection}/{itemId}`).onWrite(e => {
-  switch (e.params.collection) {
-    case 'dancers':
-    case 'contacts':
-    case 'enrollments':
-      let data = e.data.val();
-      if (data) {
-        const userIds = {};
-        userIds[e.params.userId] = e.params.userId;
+[
+  'Dancers',
+  'Contacts',
+  'Enrollments',
+].forEach((collection) => {
+  exports[`syncAdmin${collection}`] = functions.database.ref(`/${ENV}/data/users/{userId}/${collection.toLowerCase()}/{itemId}`).onWrite(debounce(10000, (e) => {
+    let data = e.data.val();
+    if (data) {
+      const userIds = {};
+      userIds[e.params.userId] = e.params.userId;
 
-        data = Object.assign({
-          '@users': userIds,
-        }, data);
-      }
-      return dba.child(`${e.params.collection}/${e.params.itemId}`).set(data);
-    default:
-      // ignore
-      return;
-  }
+      data = Object.assign({
+        '@users': userIds,
+      }, data);
+    }
+    return dba.child(`${collection.toLowerCase()}/${e.params.itemId}`).set(data);
+  }));
 });
 
-exports.sendEnrollmentSuccessEmail = functions.database.ref(`/${ENV}/data/users/{userId}/enrollments/{enrollmentId}/_submitted`).onWrite(e => {
-  if (e.data && e.data.val()) {
-    // get parent/enrollment
-    return e.data.ref.parent.once('value')
-      .then((snap) => {
-        const enrollment = snap.val();
+exports.sendEnrollmentSuccessEmail = functions.database.ref(`/${ENV}/data/users/{userId}/enrollments/{enrollmentId}/_submitted`).onWrite(debounce(10000, (e) => {
+  if (!e.data.val()) return;
+  // get parent/enrollment
+  return e.data.ref.parent.once('value')
+    .then((snap) => {
+      const enrollment = snap.val();
 
-        return getUsersEnrolledDancers(e.params.userId)
-          .then((dancers) => {
-            let ol = '<ol>';
-            dancers.forEach((dancer) => {
-              let li = '<li>';
-              li += `<strong>${getFirstName(dancer.name)}</strong>`;
-              dancer.$timeslots.forEach((timeslot) => {
-                li += `<br />${moment().day(timeslot.startDay).format('dddd')}, ${moment(timeslot.startTime, 'HH:mm').format('h:mm a')}`;
-                timeslot.$locations.forEach((location) => {
-                  li += `, ${location.nickname}`;
-                });
+      return getUsersEnrolledDancers(e.params.userId)
+        .then((dancers) => {
+          let ol = '<ol>';
+          dancers.forEach((dancer) => {
+            let li = '<li>';
+            li += `<strong>${getFirstName(dancer.name)}</strong>`;
+            dancer.$timeslots.forEach((timeslot) => {
+              li += `<br />${moment().day(timeslot.startDay).format('dddd')}, ${moment(timeslot.startTime, 'HH:mm').format('h:mm a')}`;
+              timeslot.$locations.forEach((location) => {
+                li += `, ${location.nickname}`;
               });
-              li += '</li>';
-              ol += li;
             });
-            ol += '</ol>';
+            li += '</li>';
+            ol += li;
+          });
+          ol += '</ol>';
 
-            const data = Object.assign({
-              to: `${enrollment.name} <${enrollment.email}>`,
-              subject: 'We received your enrollment  🎉 ',
+          const data = Object.assign({
+            to: `${enrollment.name} <${enrollment.email}>`,
+            subject: 'We received your enrollment  🎉 ',
 
-              title: 'Thank you for enrolling with CSHD!',
-              teaser: `Class times, start dates, costs, and more inside.`,
-              body: `<p>We're looking forward to seeing you, ${getFirstName(enrollment.name)}.</p>
-              <p>You've booked the following classes:</p>
-              ${ol}
-              <p>Classes start the week of Sept 18, 2017. You will receive another email in August with your <a href="https://campbelldancers.com/#pricing" style="color: #000000">class costs</a> and exact details.</p>
-              <p>In the meantime, if you have any questions or feedback for us, please contact Elayna at <a href="tel:+14039980111" style="color: #000000">403-998-0111</a>, or simply <a href="mailto:elayna@campbelldancers.com" style="color: #000000">reply</a> to this email.</p>
-              <p><strong>Can't wait to dance with you!</strong><br />Alexandra and Elayna</p>`,
-              button: undefined, /*{
-                text: 'Enroll',
-                url: 'https://campbelldancers.com/enroll',
-              },*/
-              image: undefined, /*{
-                src: '',
-                url: '',
-                alt: '',
-                width: 500,
-                height: 400,
-              },*/
-              
-              enrollment,
-              dancers,
-            }, e.params);
-            return renderEmail('enrollment-success', data)
-              .then((email) => {
-                return Object.assign(
-                  {},
-                  data,
-                  email,
-                  {
-                    subject: data.subject,
-                  }
-                );
-              });
-          })
-      })
-      .then((config) => {
-        console.log(config);
-        return sendEmail(config);
-      })
-      .catch((err) => console.error(err));
-  }
-});
+            title: 'Thank you for enrolling with CSHD!',
+            teaser: `Class times, start dates, costs, and more inside.`,
+            body: `<p>We're looking forward to seeing you, ${getFirstName(enrollment.name)}.</p>
+            <p>You've booked the following classes:</p>
+            ${ol}
+            <p>Classes start the week of Sept 18, 2017. You will receive another email in August with your <a href="https://campbelldancers.com/#pricing" style="color: #000000">class costs</a> and exact details.</p>
+            <p>In the meantime, if you have any questions or feedback for us, please contact Elayna at <a href="tel:+14039980111" style="color: #000000">403-998-0111</a>, or simply <a href="mailto:elayna@campbelldancers.com" style="color: #000000">reply</a> to this email.</p>
+            <p><strong>Can't wait to dance with you!</strong><br />Alexandra and Elayna</p>`,
+            button: undefined, /*{
+              text: 'Enroll',
+              url: 'https://campbelldancers.com/enroll',
+            },*/
+            image: undefined, /*{
+              src: '',
+              url: '',
+              alt: '',
+              width: 500,
+              height: 400,
+            },*/
+            
+            enrollment,
+            dancers,
+          }, e.params);
+          return renderEmail('enrollment-success', data)
+            .then((email) => {
+              return Object.assign(
+                {},
+                data,
+                email,
+                {
+                  subject: data.subject,
+                }
+              );
+            });
+        })
+    })
+    .then((config) => {
+      console.log(config);
+      return sendEmail(config);
+    })
+    .catch((err) => console.error(err));
+}));
