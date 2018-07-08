a9fb3c3bbe5e1516dde5268829f20870befd6ae8
Murray Smith <murray@mismith.info>
2018-07-08 01:46:24 -0600

    bcc self on all emails out

--- functions/helpers/sendEmail.js
+++ functions/helpers/sendEmail.js
@@ -2,6 +2,7 @@ const functions = require('firebase-functions');
 const sendEmail = require('gmail-send')({
   user: functions.config()['gmail-send'].user,
   pass: functions.config()['gmail-send'].pass,
+  bcc: 'hello@campbelldancers.com',
   from: 'Campbell School of Highland Dance <hello@campbelldancers.com>',
   replyTo: 'Campbell School of Highland Dance <hello@campbelldancers.com>',
 });
